7cc8ec316d1ab578b3273eafce7c91d54fa4a6b1
Sarronnn <stadesse@lion.lmu.edu>
2022-11-06 14:42:49 -0800

    Sign-in not added

---
+++ src/firebaseConfig.js
@@ -0,0 +1,19 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore /* and maybe other things */ } from "firebase/firestore";
+import { getAuth /* and maybe other things */ } from "firebase/auth";
+// ... and do the same for other services you need
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCz6IZZBmzuXwOAHTDaxIq66QU3ewqhmx0",
+  authDomain: "blog-d4600.firebaseapp.com",
+  projectId: "blog-d4600",
+  storageBucket: "blog-d4600.appspot.com",
+  messagingSenderId: "695093946474",
+  appId: "1:695093946474:web:dee1cd4267cfa79c320213",
+};
+export const app = initializeApp({
+  firebaseConfig,
+});
+export const db = getFirestore(app);
+export const auth = getFirestore(app);
+// ... and do the same for other services you need
